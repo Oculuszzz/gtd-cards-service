e6def4f4a83935e2df11178af3754af6ce8b6f1e
Shahir <mokhtarsha94@gmail.com>
2026-04-13 05:40:22 +0800

    chore: initial spring boot project setup

---
+++ src/main/java/com/shahir/gtdcards/GtdcardsApplication.java
@@ -0,0 +1,13 @@
+package com.shahir.gtdcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdcardsApplication.class, args);
+	}
+
+}
